79d44a5fa147ed3727f2931249cba76b7202fc01
Encep Suryana <encep.suryanajr@gmail.com>
2021-09-06 15:58:37 +0700

    Add Configure Midtrans Controllers

---
+++ app/Http/Controllers/API/MidtransController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use Midtrans\Config;
+use Midtrans\Notification;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Models\Transaction;
+
+class MidtransController extends Controller
+{
+    public function callback(Request $request) {
+        // Set Konfigurasi Midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        // Buat Instance Midtrans Nigification
+        $notification = new Notification();
+
+        // Asign ke Variable untuk memudahkan coding
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $froud = $notification->froud_status;
+        $order_id = $notification->order_id;
+
+        // Cari Transaksi berdasarkan ID
+        $transaction = Transaction::findOrFail($order_id);
+
+        // Handle notifikasi status midtrans
+        if ($status == 'capture') {
+            if ($type == 'credit_card') {
+                if ($froud == 'challenge') {
+                    $transaction->status ="PENDING";
+                } else {
+                    $transaction->status ="SUCCESS";
+
+                }
+            }
+        } else if ($status == 'settlement') {
+            $transaction->status ="SUCCESS";
+
+        } else if ($status == 'pending') {
+            $transaction->status ="PENDING";
+
+        } else if ($status == 'deny') {
+            $transaction->status ="CANCELED";
+
+        } else if ($status == 'expire') {
+            $transaction->status ="CANCELED";
+            
+        } else if ($status == 'cancel') {
+            $transaction->status ="CANCELED";
+            
+        }
+
+        // Simpan Transaksi
+
+        $transaction->save();
+    }
+}
--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -121,4 +121,6 @@ class TransactionController extends Controller
 
 
     }
+
+    
 }
